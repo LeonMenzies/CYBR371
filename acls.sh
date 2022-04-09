9a48c986052302e478b71e9fe7e60e788d43a819
Leon Menzies <leon.menzies@hotmail.com>
2022-04-09 19:07:21 +1200

    Update acls.sh

--- acls.sh
+++ acls.sh
@@ -15,5 +15,39 @@ setfacl -m g:Receptionists:rwx /opt/WellingtonClinic/patients
 
 
 #Setting file ACLs
-setfacl -m g:Administrators:rwx /opt/WellingtonClinic
-setfacl -m g:Administrators:rwx /opt/WellingtonClinic
+
+#Administrators
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/sbasicinfo.log
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/pbasicinfo.log
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/pmedicalrecord.log
+setfacl -m g:Administrators:rwx /opt/file-system-setup.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/staff-create.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/acls.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/register-patient.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/assign-doc.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/search-patient.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/search-doctor.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/visit.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/check-medication.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/audit.sh
+
+#Administrators
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/sbasicinfo.log
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/pbasicinfo.log
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/pmedicalrecord.log
+setfacl -m g:Administrators:rwx /opt/file-system-setup.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/staff-create.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/acls.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/register-patient.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/assign-doc.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/search-patient.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/search-doctor.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/visit.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/check-medication.sh
+setfacl -m g:Administrators:rwx /opt/WellingtonClinic/scripts/audit.sh
+
+
+
+
+
+
