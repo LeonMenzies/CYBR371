2a7a0136ccf744b937a3fcb720c6a838b6962819
Leon Menzies <leon.menzies@hotmail.com>
2022-04-09 20:23:57 +1200

    Update assign-doc.sh

--- assign-doc.sh
+++ assign-doc.sh
@@ -1,1 +1,20 @@
+!# /user/bin/basg
 
+echo "Patient name:"
+read patient_name
+
+echo "Patient birthdate (DD/MM/YYYY):"
+read patient_birthday
+
+echo "Doctors username:"
+read assigned_doctor
+
+#Create the username
+first_let = ${patient_name:0:1} 
+last_let = ${patient_name:`expr "$patient_name" : '.*'`:1} 
+
+
+username = first_let + last_let + patient_birthday | sed 's/\///'
+
+#Add info to pbasicinfo
+echo username
