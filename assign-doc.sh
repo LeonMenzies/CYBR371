085f7b71bd861696d33cd89f1595a7efe1cfba28
Leon Menzies <leon.menzies@hotmail.com>
2022-04-09 21:25:36 +1200

    Update assign-doc.sh

--- assign-doc.sh
+++ assign-doc.sh
@@ -6,6 +6,9 @@ read patient_name
 echo "Patient birthdate (DD/MM/YYY):"
 read patient_birthday
 
+echo "Doctors username:"
+read assigned_doctor
+
 username="${patient_name:0:1}${patient_name:`expr "$patient_name" : '.*'`-1:1}${patient_birthday///}"
 
-echo $username
+echo $assigned_doctor >> /opt/WellingtonClinic/patients/$username/pbasicinfo.log
