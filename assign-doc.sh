5ebf69b9a89f8091f8c92d51835a8e3b16636eb3
Leon Menzies <leon.menzies@hotmail.com>
2022-04-09 21:16:51 +1200

    Update assign-doc.sh

--- assign-doc.sh
+++ assign-doc.sh
@@ -3,18 +3,9 @@
 echo "Patient name:"
 read patient_name
 
-echo "Patient birthdate (DD/MM/YYYY):"
+echo "Patient birthdate (DD/MM/YYY):"
 read patient_birthday
 
-echo "Doctors username:"
-read assigned_doctor
+username="${patient_name:0:1}${patient_name:`expr "$patient_name" : '.*'`-1:1}${patient_birthday///}"
 
-#Create the username
-first_let = ${patient_name:0:1} 
-last_let = ${patient_name:`expr "$patient_name" : '.*'`:1} 
-
-
-username = first_let + last_let + patient_birthday | sed 's/\///'
-
-#Add info to pbasicinfo
-echo username
+echo $username
