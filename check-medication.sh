43d4b0f43eec9c815e26138f134d402c239f563e
Leon Menzies <leon.menzies@hotmail.com>
2022-04-11 01:40:15 +1200

    Update check-medication.sh

--- check-medication.sh
+++ check-medication.sh
@@ -13,6 +13,10 @@ username="${patient_name:0:1}${patient_name:`expr "$patient_name" : '.*'`-1:1}${
 #Convert string to array
 #mapfile -t -d ',' arr < ../patients/${username,,}/pmedicalrecord.log
 
+echo "Doctor     Medication"
+
 while read p; do
-  echo "$p"
+    IFS=', ' read -r -a arr <<< $p
+    echo "${arr[1]}        ${arr[4]}"
+
 done <WellingtonClinic/patients/${username,,}/pmedicalrecord.log
