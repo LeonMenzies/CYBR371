3568f708d76a78a3bf8e6e8fbaa7ffc05c16787d
Leon Menzies <leon.menzies@hotmail.com>
2022-04-10 21:09:18 +1200

    Update register-patient.sh

--- register-patient.sh
+++ register-patient.sh
@@ -1,29 +1,33 @@
-echo "Enter the following information about the patient:"
-echo "First name: "
+#! /user/bin/bash
+
+#Collect information
+echo "First name:"
 read first_name
-echo "Last name: "
+echo "Last name:"
 read last_name
-echo "Date of birth (23/12/2001): "
+echo "Date of birth (DD/MM/YYY):"
 read dob
-echo "Gender: "
+echo "Gender:"
 read gender
-echo "Physical address: "
-read address
-echo "Email: "
+echo "Physical address:"
+read physical_address
+echo "Email:"
 read email
-echo "Phone number: "
+echo "Phone number:"
 read phone_number
-echo "Registered doctors (enter username seperated by comma (,): "
+echo "Registered doctors (enter username seperated by comma (,):"
 read registered_doctors
 
 # Need to implement doctors not being able to add themselves as a registered
 # doctor to their own patient file
 
+#Create the username
 username="${first_name:0:1}${last_name:`expr "$last_name" : '.*'`-1:1}${dob///}"
 
+#Method for making a directory andfile for each patient 
 make_patient(){
     mkdir /opt/WellingtonClinic/patients/$1
     echo $2 >> /opt/WellingtonClinic/patients/$1/pbasicinfo.log
 }
 
-make_patient $username "$first_name,$last_name,$dob,$gender,$address,$email,$registered_doctors"
+make_patient $username "$first_name,$last_name,$dob,$gender,$physical_address,$email,$registered_doctors"
