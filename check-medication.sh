d9e85e9c144399123630fd4417383b6963f19d89
Leon Menzies <leon.menzies@hotmail.com>
2022-04-11 16:46:19 +1200

    Update check-medication.sh

--- check-medication.sh
+++ check-medication.sh
@@ -10,13 +10,12 @@ read patient_birthday
 #Create username
 username="${patient_name:0:1}${patient_name:`expr "$patient_name" : '.*'`-1:1}${patient_birthday///}"
 
-#Convert string to array
-mapfile -t -d ',' arr < ../patients/${username,,}/pmedicalrecord.log
 
 echo "Doctor          Medication"
 
+#Check each line of the medicial record
 while read p; do
     IFS=',' read -r -a arr <<< $p
     echo "${arr[1]}        ${arr[3]}"
 
-done <WellingtonClinic/patients/${username,,}/pmedicalrecord.log
+done </optWellingtonClinic/patients/${username,,}/pmedicalrecord.log
