312680caa7da116c400817b3eb817ac30c0f1874
Leon Menzies <leon.menzies@hotmail.com>
2022-04-11 15:17:02 +1200

    Update searchpatient.sh

--- searchpatient.sh
+++ searchpatient.sh
@@ -4,11 +4,12 @@ echo -n "Doctors username: "
 read doc_username
 
 echo "Doctor             Patients"
+echo -n "${doc_username}            "
 
 #Iterate each patient
 for d in ../patients/* ; do
 #check if they ar assigned to given doctor
-    if grtep $doc_username $d/pbasicinfo.log ; then
+    if grep -q $doc_username "$d/pbasicinfo.log" ; then
     echo "test"
     
 done
