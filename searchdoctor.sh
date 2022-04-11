c998549140d7c773d44aa82ba371014b3c1378fa
Leon Menzies <leon.menzies@hotmail.com>
2022-04-11 16:48:07 +1200

    Update searchdoctor.sh

--- searchdoctor.sh
+++ searchdoctor.sh
@@ -11,7 +11,7 @@ read patient_birthday
 username="${patient_name:0:1}${patient_name:`expr "$patient_name" : '.*'`-1:1}${patient_birthday///}"
 
 #Convert string to array
-mapfile -t -d ',' arr < ../patients/${username,,}/pbasicinfo.log
+mapfile -t -d ',' arr < /opt/WellingtonClinic/patients/${username,,}/pbasicinfo.log
 
 #Print results
 echo "Patient                Doctors"
