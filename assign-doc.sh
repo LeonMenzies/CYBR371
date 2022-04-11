83192ccd84bcf7fe0b85442bf0cdcf7233365b64
Leon Menzies <leon.menzies@hotmail.com>
2022-04-11 15:18:40 +1200

    Update assign-doc.sh

--- assign-doc.sh
+++ assign-doc.sh
@@ -12,4 +12,8 @@ read assigned_doctor
 
 username="${patient_name:0:1}${patient_name:`expr "$patient_name" : '.*'`-1:1}${patient_birthday///}"
 
+#Add the doctor to the list
 echo -n ",${assigned_doctor}" >> /opt/WellingtonClinic/patients/${username,,}/pbasicinfo.log
+
+#Set the doctors acl permissions
+setfacl -m u:$username:rw /opt/WellingtonClinic/patients/${username,,}/pmedicalrecord.log
