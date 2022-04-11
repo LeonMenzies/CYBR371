5d1c1a680689a6418e543170e986574f9c808251
LeonMenzies <leon.menzies@hotmail.com>
2022-04-11 04:15:33 +0000

    finished 8

--- searchpatient.sh
+++ searchpatient.sh
@@ -4,12 +4,16 @@ echo -n "Doctors username: "
 read doc_username
 
 echo "Doctor             Patients"
-echo -n "${doc_username}            "
+echo -n "${doc_username}           "
 
 #Iterate each patient
-for d in ../patients/* ; do
-#check if they ar assigned to given doctor
-    if grep -q $doc_username "$d/pbasicinfo.log" ; then
-    echo "test"
+for d in /opt/WellingtonClinic/patients/*; do
+    #check if they are assigned to given doctor
+    if grep -q $doc_username $d/pbasicinfo.log ; then
     
+    #print hte patients names
+    mapfile -t -d ',' arr < $d/pbasicinfo.log
+    echo -n "${arr[@]::2},"
+    fi
 done
+echo
