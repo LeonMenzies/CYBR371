87aa3ddb6aaacab97fdf5209fd50ef7c8dc9a259
Leon Menzies <leon.menzies@hotmail.com>
2022-04-12 19:38:03 +1200

    Update staff-create.sh

--- staff-create.sh
+++ staff-create.sh
@@ -11,12 +11,6 @@ add_user(){
 
     #Add the user 
     useradd -g $3 $1
-
-    #Set there passwords
-    passwd $1
-    
-    echo 123
-    echo 123
     
     mkdir /opt/WellingtonClinic/staff/$2/$1 
 
