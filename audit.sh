f9ba26268c10d62f3c2c72993c28b60da8c38dda
Leon Menzies <leon.menzies@hotmail.com>
2022-04-13 19:35:46 +1200

    Update audit.sh

--- audit.sh
+++ audit.sh
@@ -3,6 +3,6 @@
 #Read each line of the log file
 while read p; do
     IFS=',' read -r -a arr <<< $p
-    echo "${arr[0]},${arr[1]}        ${arr[2]}        ${arr[3]}"
+    echo $p
 
 done </opt/WellingtonClinic/scripts/audit.log
