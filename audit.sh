22b562515ec2f8a5f9da5483d2f09b75032238d1
Leon Menzies <leon.menzies@hotmail.com>
2022-04-13 17:19:10 +1200

    Update audit.sh

--- audit.sh
+++ audit.sh
@@ -1,2 +1,8 @@
 #! usr/bin/bash
 
+#Read each line of the log file
+while read p; do
+    IFS=',' read -r -a arr <<< $p
+    echo "${arr[0]}        ${arr[1]}        ${arr[2]}        ${arr[3]}"
+
+done </opt/WellingtonClinic/patients/scripts/audit.log
